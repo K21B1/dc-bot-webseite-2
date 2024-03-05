c36b475f3aa11aab4f335341123114442661c4c6
Jakub Manka <jakub.manka99@gmail.com>
2024-03-05 16:08:53 +0100

    pepo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     <div className="text-white bg-gray-950 h-screen">
       <div>
         <a className="text-7xl font-bold">GetOnMyLVL</a>
+        <a className="text-7xl font-bold">GetOnMyLVL</a>
       </div>
     </div>
   );
