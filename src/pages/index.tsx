a506509758e9a8deab158778c85212f0d47a94c9
Jakub Manka <jakub.manka99@gmail.com>
2024-03-05 16:07:10 +0100

    +++

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="text-white">
+    <div className="text-white bg-gray-950 h-screen">
       <div>
-        <a>lolo
-        </a>
+        <a className="text-7xl font-bold">GetOnMyLVL</a>
       </div>
     </div>
   );
